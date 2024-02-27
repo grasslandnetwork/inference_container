195a3beb165fd1de2a63444a64a9548997a5b591
00hello <445986+00hello@users.noreply.github.com>
2024-02-26 22:39:22 -0500

    Start mediamtx server in background on startup

---
+++ pipeless/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+. /scripts/libpipeless.sh
+
+command="${1:-}"
+
+# Setup Python virtual environment
+# the user can mount a volume at /.venv and avoid the installation of the Python packages on every start
+echo "Creating Python virtual env..."
+python3 -m venv "/.venv"
+# Activate Python venv
+. "/.venv/bin/activate"
+
+if [[ "$command" = "start" ]]; then
+    pipeless_install_user_python_deps
+    # Start mediamtx server here
+    echo "Starting mediamtx server..."
+    /usr/local/bin/mediamtx & # Start mediamtx in the background
+fi
+
+if [[ "$command" = "pipeless" ]]; then
+    exec "$@"
+else
+    exec pipeless "$@"
+fi
+
+
